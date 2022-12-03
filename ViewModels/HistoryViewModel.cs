fc336e43b4ac58ec0a96db8bc89214e4c1e2f282
Kartheek <bxmeda@shockers.wichita.edu>
2022-12-02 18:31:05 -0600

    Changed ViewModel to use database functions.

--- ViewModels/HistoryViewModel.cs
+++ ViewModels/HistoryViewModel.cs
@@ -1,29 +1,39 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using System.Collections.ObjectModel;
+using Calculator.Models;
+using Calculator.Services;
 
 namespace Calculator.ViewModels
 {
-    public partial class HistoryViewModel:ObservableObject
+    public partial class HistoryViewModel : ObservableObject
     {
         public HistoryViewModel()
         {
-            Expr = new ObservableCollection<string>();
+            Expressions = new ObservableCollection<HistoryModel>();
+            //getAllExpr();
         }
 
         [ObservableProperty]
-        ObservableCollection<string> expr;
+        public static ObservableCollection<HistoryModel> expressions;
 
         public static string exprString = "";
 
         [RelayCommand]
-        public void AddExpr()
+        public static async void AddExpr(string a)
         {
-            if (string.IsNullOrEmpty(exprString))
+            if (string.IsNullOrEmpty(a))
             {
                 return;
             }
-            Expr.Add($" ⟹ {exprString}");
+            await HistoryService.AddExpr(a);
+
+            ObservableCollection<HistoryModel> result = await HistoryService.GetExpr();
+
+            System.Diagnostics.Debug.WriteLine("THESE ARE THE EXPRESSIONS", result);
+
         }
+
+
     }
 }
