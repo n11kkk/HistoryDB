7f64cd4f481df3c9b67dbfcafeede8c27a0943e7
Nikilesh <nxsambamurthy@shockers.wichita.edu>
2022-12-02 18:03:44 -0600

    Addede the History Model.

---
+++ Models/HistoryModel.cs
@@ -0,0 +1,17 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.Models
+{
+    [Table("CalculatorHistory")]
+    public class HistoryModel
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string Expr { get; set; }
+    }
+}
